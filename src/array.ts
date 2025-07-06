8f83b65f32ca8adda6e1d148ba4e7a2c76bab7df
Ryan <ryanbprog@gmail.com>
2025-07-06 22:47:35 +0100

    push, pop and get methods and tests added

--- src/array.ts
+++ src/array.ts
@@ -9,4 +9,29 @@ export default class MyArray<T> {
   values() {
     return Object.values(this.items);
   }
+
+  push(value: T) {
+    this.items[this._length] = value;
+    this._length++;
+  }
+
+  pop() {
+    if (this._length === 0) return undefined;
+    const val = this.items[this._length - 1];
+    delete this.items[this._length - 1];
+    this._length--;
+    return val;
+  }
+
+  get(index: number) {
+    if (typeof index !== "number") {
+      throw new Error(
+        "The given argument is not a number, it needs to be of type number",
+      );
+    }
+    if (index > this._length - 1) {
+      throw new Error("Given index out of range");
+    }
+    return this.items[index];
+  }
 }
--- tests/array.test.ts
+++ tests/array.test.ts
@@ -1,13 +1,42 @@
 import MyArray from "../src/array";
 
 describe("MyArray", () => {
-  test("should initialize with length 0", () => {
+  test("array should initialize with the length at 0", () => {
     const arr = new MyArray();
     expect(arr.length).toBe(0);
   });
 
-  test("add a value to the array with pop method", () => {
+  test("checking the values method returns correctly", () => {
     const arr = new MyArray();
     expect(arr.values()).toEqual([]);
+  });
+
+  test("add a value to the array with the push method", () => {
+    const arr = new MyArray();
+    arr.push("testing...");
+    expect(arr.values()).toEqual(["testing..."]);
+    expect(arr.length).toBe(1);
+  });
+
+  test("remove a value from the array with the pop method", () => {
+    const arr = new MyArray();
+    arr.push(1);
+    arr.push("remove");
+    const removed = arr.pop();
+    expect(arr.length).toBe(1);
+    expect(removed).toBe("remove");
+    expect(arr.values()).toEqual([1]);
+    arr.pop();
+    arr.pop();
+    expect(arr.values()).toEqual([]);
+  });
+
+  test("get method to return the value of a specified index", () => {
+    const arr = new MyArray();
+    arr.push("first");
+    arr.push("second");
+    arr.push("third");
+    expect(arr.get(1)).toEqual("second");
+    expect(() => arr.get(5)).toThrow("Given index out of range");
   });
 });
